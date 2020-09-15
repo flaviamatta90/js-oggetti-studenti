8dc002ef02189c898724380e8c79fda90363e0cf
Flavia <flaviamatta90@gmail.com>
2020-09-15 14:39:57 +0200

    console.log print 2° ex

--- js/script.js
+++ js/script.js
@@ -10,10 +10,50 @@ $(document).ready(
     };
 
     for (var key in proprietà){
-      console.log(key + " " + proprietà[key]);
+      // console.log(key + " " + proprietà[key]);
     }
-    
+
     // 2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+    var studenti =  [
+  {
+    'Nome' : 'Alberto',
+    'Cognome' : 'Bianchi',
+  },
+
+  {
+    'Nome' : 'Antonio',
+    'Cognome' : 'Rossi',
+  },
+
+  {
+    'Nome' : 'Angela',
+    'Cognome' : 'Neri',
+  },
+
+  {
+    'Nome' : 'Bianca',
+    'Cognome' : 'Bianchi',
+  },
+
+  {
+    'Nome' : 'Azzurra',
+    'Cognome' : 'Di Battisti',
+  },
+
+  {
+    'Nome' : 'Celeste',
+    'Cognome' : 'Verdi',
+  },
+];
+
+  for(var i = 0; i < studenti.length; i++){
+
+    for (var key in studenti[i]){
+
+      console.log(key, studenti[i][key]);
+
+    }
+  };
     // 3 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 
@@ -28,4 +68,4 @@ $(document).ready(
 
 
   }
-);
+  );
