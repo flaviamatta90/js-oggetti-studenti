d0de8be2f8bd55c110fa54c13e189d9ab7654aec
Flavia <flaviamatta90@gmail.com>
2020-09-15 15:45:24 +0200

    parseint + age

--- js/script.js
+++ js/script.js
@@ -2,60 +2,74 @@
 $(document).ready(
   function (){
 
+    // stampare a video con handbarbs
+    // var source = document.getElementById("entry-template").innerHTML;
+    // var template = Handlebars.compile(source);
+    //
+    // var html = template(proptieta);
+
     // 1 Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
-    var proprietà = {
+
+    var proprieta = {
       'Nome' : 'Flavia',
       'Cognome' : 'Matta',
-      'Eta' : 30
+      'Eta' : 30,
     };
 
-    for (var key in proprietà){
+    for (var key in proprieta){
       // console.log(key + " " + proprietà[key]);
     }
 
-    // 2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 
+    // 2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
     // 3 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
     var nomeUtente = prompt("Inserisci il nome dello studente")
     var cognomeUtente = prompt("Inserisci il cognome dello studente")
-    var etaUtente = prompt("Inserisci l'età dello studente");
+    var etaUtente = parseInt(prompt("Inserisci l'età dello studente"));
 
     var studenti =  [
   {
     'Nome' : 'Alberto',
     'Cognome' : 'Bianchi',
+    'Eta' : 21,
   },
 
   {
     'Nome' : 'Antonio',
     'Cognome' : 'Rossi',
+    'Eta' : 36,
   },
 
   {
     'Nome' : 'Angela',
     'Cognome' : 'Neri',
+    'Eta' : 27,
+
   },
 
   {
     'Nome' : 'Bianca',
     'Cognome' : 'Bianchi',
+    'Eta' : 24,
   },
 
   {
     'Nome' : 'Azzurra',
     'Cognome' : 'Di Battisti',
+    'Eta' : 33,
   },
 
   {
     'Nome' : 'Celeste',
     'Cognome' : 'Verdi',
+    'Eta' : 34,
   },
 
   {
     'Nome' : nomeUtente,
     'Cognome' : cognomeUtente,
-    'eta' : etaUtente,
+    'Eta' : etaUtente,
   },
 ];
 
@@ -67,14 +81,6 @@ $(document).ready(
 
     }
   };
-
-
-    // 3 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-
-
-
-
-
 
 
 
