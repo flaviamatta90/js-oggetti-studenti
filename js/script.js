779295d9abedcd758ceddbf7ab9b5a9b97fe762c
Flavia <flaviamatta90@gmail.com>
2020-09-15 14:31:23 +0200

    console.log print 1° ex

--- js/script.js
+++ js/script.js
@@ -2,9 +2,19 @@
 $(document).ready(
   function (){
 
-    1 Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
-    2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
-    3 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+    // 1 Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+    var proprietà = {
+      'Nome' : 'Flavia',
+      'Cognome' : 'Matta',
+      'Eta' : 30
+    };
+
+    for (var key in proprietà){
+      console.log(key + " " + proprietà[key]);
+    }
+    
+    // 2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+    // 3 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 
 
